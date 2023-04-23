44b3cbc27a38cddef5494819af227d4b71ea4c58
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-04-23 20:58:02 +0200

    format code

--- src/menhely/Main.java
+++ src/menhely/Main.java
@@ -1,14 +1,16 @@
 package menhely;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static Allat[] allatok= new Allat[20];
+	static Allat[] allatok = new Allat[20];
 	static int aktualiyAllatSzam = 0;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-Menu menu = new Menu();
-menu.show(br,allatok,aktualiyAllatSzam);
+		Menu menu = new Menu();
+		menu.show(br, allatok, aktualiyAllatSzam);
 	}
 }
